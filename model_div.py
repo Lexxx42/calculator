9db11014185826e3d11b92cdbdf42d98c65ad43d
Evgeniya <ponevgeniya@mail.ru>
2022-12-14 11:52:28 +0500

    Написали функцию

---
+++ model_div.py
@@ -0,0 +1,10 @@
+a = float (input("Введите действительную часть числа a: "))
+b = float (input(" Введите мнимую часть числа b: "))
+c = float (input("Введите действительную часть числа c: "))
+d = float (input(" Введите мнимую часть числа d: "))
+
+def div_complex_number(delimoe: complex, delitel: complex):
+    rez = delimoe / delitel
+    return rez
+
+
