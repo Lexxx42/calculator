7760cf91f7cff5b6e22253657e6f8670f2b21a70
Evgeniya <ponevgeniya@mail.ru>
2022-12-14 12:12:11 +0500

    Добавлен комментарий: вычисление рациональных чисел

--- model_div.py
+++ model_div.py
@@ -14,5 +14,18 @@ rez = div_complex_number(delimoe,delitel)
 
 
 
+# Вычисление рациональных чисел
 
+delimoe = float(input("Введите делимое m: "))
+delitel = float(input("Введите делитель n: "))
 
+def div_rational_number(delimoe: float, delitel: float):
+    
+    if delitel == 0:
+        print("Ошибка, на ноль делить нельзя")
+        return 0
+
+    else:
+        return delimoe / delitel
+
+rez = div_rational_number(delimoe,delitel)    
