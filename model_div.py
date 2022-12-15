6226513d69ea8db5c3574170c5d87214497b7a35
Alexander Konukhov <jesusmisha@gmail.com>
2022-12-16 00:10:16 +0300

    global auto formatting ctrl+alt+L

--- model_div.py
+++ model_div.py
@@ -1,9 +1,9 @@
 # Вычисление комплексных чисел
 
-#a = float(input("Введите действительную часть числа a: "))
-#b = float(input(" Введите мнимую часть числа b: "))
-#c = float(input("Введите действительную часть числа c: "))
-#d = float(input(" Введите мнимую часть числа d: "))
+# a = float(input("Введите действительную часть числа a: "))
+# b = float(input(" Введите мнимую часть числа b: "))
+# c = float(input("Введите действительную часть числа c: "))
+# d = float(input(" Введите мнимую часть числа d: "))
 
 
 def div_complex_number(delimoe: complex, delitel: complex):
@@ -16,22 +16,22 @@ delitel = complex(c, d)
 
 rez = div_complex_number(delimoe, delitel)
 
+
 # Вычисление рациональных чисел
 
-#delimoe = float(input("Введите делимое m: "))
-#delitel = float(input("Введите делитель n: "))
+# delimoe = float(input("Введите делимое m: "))
+# delitel = float(input("Введите делитель n: "))
 
 
 def div_rational_number(delimoe: float, delitel: float):
-    #if delitel == 0:
+    # if delitel == 0:
     #    print("Ошибка, на ноль делить нельзя")
     #    return 0
 
-    #else:
+    # else:
     return delimoe / delitel
 
 
 rez = div_rational_number(delimoe, delitel)
-
 
 # missing options: //, % for real numbers
