a2f36c6ccdb051ad7ff4a9206bd5594bf4ea29d0
Alexandr Konukhov <jesusmisha@gmail.com>
2022-12-16 14:57:18 +0300

    compl.py line 4 added critical notice comment

--- compl.py
+++ compl.py
@@ -1,6 +1,7 @@
 def to_complex(number: dict) -> complex:
     for key, value in number.items():
         comple = complex(int(key), int(value))
+        # Почему тут инты? 0.8909 + i*0.8909 - не валидное комплексное число? - int(0.15) = 0. мы будем деление на ноль делать в обход эсепшенов!
     return comple
 
 # def init(a,aj) -> complex:
